0f6522db0ea0c1a59aa43552223b0ec3fbe87f93
alessio.pace <alessio.pace@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-21 14:54:03 +0000

    classes to be used for testing component and interfaces detection

---
+++ trunk/src/test/java/org/objectweb/fractal/fractalizer/ServiceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.objectweb.fractal.fractalizer;
+
+/**
+ * @author Alessio Pace
+ *
+ */
+public class ServiceImpl implements Service {
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.fractal.fractalizer.Service#doSomething()
+	 */
+	public void doSomething() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/src/test/java/org/objectweb/fractal/fractalizer/ServiceImpl2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.objectweb.fractal.fractalizer;
+
+/**
+ * @author Alessio Pace
+ *
+ */
+public class ServiceImpl2 implements Service {
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.fractal.fractalizer.Service#doSomething()
+	 */
+	public void doSomething() {
+		// 
+	}
+
+}
