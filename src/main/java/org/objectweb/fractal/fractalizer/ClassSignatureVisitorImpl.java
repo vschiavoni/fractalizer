5e7f86c5dc3a9a30d584922f073ca4f715d5d036
valerio.schiavoni <valerio.schiavoni@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-21 14:51:04 +0000

    empty test case, interface and impl for ClassSignatureVisitor

---
+++ src/main/java/org/objectweb/fractal/fractalizer/ClassSignatureVisitorImpl.java
@@ -0,0 +1,24 @@
+/**
+ * Author: Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ */
+package org.objectweb.fractal.fractalizer;
+
+
+/**
+ * @author Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ *
+ */
+public class ClassSignatureVisitorImpl implements ClassSignatureVisitor
+{
+  
+  
+  /**
+   * @see org.objectweb.fractal.fractalizer.ClassSignatureVisitor#visit(java.lang.Class)
+   */
+  public void visit(Class<?> clazz)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
