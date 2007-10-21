cdef8a0dc8eb680cd04b34d3ef860e5c2bdb55d7
valerio.schiavoni <valerio.schiavoni@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-21 15:25:29 +0000

    a factory for nodes in the graph

---
+++ trunk/src/main/java/org/objectweb/fractal/fractalizer/graph/GraphNodeFactory.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ */
+package org.objectweb.fractal.fractalizer.graph;
+
+/**
+ * @author Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ *
+ */
+public interface GraphNodeFactory
+{
+  PrimitiveComponentNode createPrimitiveComponentNode();
+}
---
+++ trunk/src/main/java/org/objectweb/fractal/fractalizer/graph/GraphNodeFactoryImpl.java
@@ -0,0 +1,22 @@
+/**
+ * Author: Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ */
+package org.objectweb.fractal.fractalizer.graph;
+
+/**
+ * @author Valerio Schiavoni <valerio.schiavoni@gmail.com>
+ *
+ */
+public class GraphNodeFactoryImpl implements GraphNodeFactory
+{
+
+  /**
+   * @see org.objectweb.fractal.fractalizer.graph.GraphNodeFactory#createPrimitiveComponentNode()
+   */
+  public PrimitiveComponentNode createPrimitiveComponentNode()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
