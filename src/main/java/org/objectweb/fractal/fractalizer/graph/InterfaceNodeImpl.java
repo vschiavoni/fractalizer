b9911ac621c3e124c4b529e3e84ac71ea00aa344
alessio.pace <alessio.pace@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-21 15:40:26 +0000

    added method to retrieve the component owner of an interface

--- src/main/java/org/objectweb/fractal/fractalizer/graph/InterfaceNodeImpl.java
+++ src/main/java/org/objectweb/fractal/fractalizer/graph/InterfaceNodeImpl.java
@@ -9,19 +9,37 @@ package org.objectweb.fractal.fractalizer.graph;
  */
 public class InterfaceNodeImpl implements InterfaceNode {
 
+	private PrimitiveComponentNode owner;
 	private String name;
 	private String signature;
 	
-	public InterfaceNodeImpl(String name, String signature) {
+	public InterfaceNodeImpl(PrimitiveComponentNode owner, String name, String signature) {
+		this.owner = owner;
 		this.name = name;
 		this.signature = signature;
 	}
-	
-	public final String getName() {
+
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.fractal.fractalizer.graph.InterfaceNode#getOwner()
+	 */
+	public PrimitiveComponentNode getOwner() {
+		return this.owner;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.fractal.fractalizer.graph.InterfaceNode#getName()
+	 */
+	public String getName() {
 		return this.name;
 	}
 
-	public final String getSignature() {
+
+	/* (non-Javadoc)
+	 * @see org.objectweb.fractal.fractalizer.graph.InterfaceNode#getSignature()
+	 */
+	public String getSignature() {
 		return this.signature;
 	}
 
