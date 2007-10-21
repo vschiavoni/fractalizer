278463a4c7ca789679899cd3c03d38dc1116043b
valerio.schiavoni <valerio.schiavoni@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-21 15:12:42 +0000

    add failing test

--- src/main/java/org/objectweb/fractal/fractalizer/ClassSignatureVisitor.java
+++ src/main/java/org/objectweb/fractal/fractalizer/ClassSignatureVisitor.java
@@ -1,6 +1,8 @@
 
 package org.objectweb.fractal.fractalizer;
 
+import org.objectweb.fractal.fractalizer.graph.ComponentGraph;
+
 /**
  * Inspect  a class signature
  */
@@ -8,5 +10,13 @@ public interface ClassSignatureVisitor
 {
   
   public void visit(Class<?> clazz);
+
+  public void visit(Class<?>[] clazzes);
+
+  /**
+   * @return the component graph
+   */
+  public ComponentGraph getComponentGraph();
+  
   
 }
