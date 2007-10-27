53942c261301b5f5344e1df77203ee960cc8143e
valerio.schiavoni <valerio.schiavoni@45c9210c-013d-0410-8ae9-79ad85499798>
2007-10-27 15:33:59 +0000

    Start support input from JAR file (see issue 8); add assembly plugin (see issue 9)

---
+++ src/main/java/org/objectweb/fractal/fractalizer/Fractalizer.java
@@ -0,0 +1,96 @@
+
+package org.objectweb.fractal.fractalizer;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Fractalizer is a best-effort tool to automatically transform an Object-Oriented java application
+ * to a corresponding Component-Oriented java application, according to the Fractal Component Model.
+ *
+ */
+public class Fractalizer
+{
+
+  public String fractalize(String jarName) {
+    ClassVisitor visitor = new ClassVisitorImpl();
+    
+    JarClassLoader jarLoader = new JarClassLoader(jarName);
+    
+    for (String clazz: jarLoader.classes()) {
+      if (!clazz.endsWith(".class")) {
+        System.err.println("Skipping file: "+clazz);
+        continue;
+      }
+      try
+      {
+        Class<?> c = jarLoader.loadClass(clazz, true); //true to resolve the class, required to use it.
+        visitor.visit(c);
+      }
+      catch (ClassNotFoundException e)
+      {
+        System.err.println("Could not load class with name: "+clazz);
+        e.printStackTrace();
+        
+      }
+    }
+    
+    ADLWriterGraphVisitor writer = new ADLWriterGraphVisitorImpl();
+   
+    return writer.visit(visitor.getComponentGraph());
+  }
+  
+  public void writeAdlOn(String adlContent, String adlFileName) throws IOException {
+    FileUtils.writeStringToFile(new File(adlFileName), adlContent);
+  }
+  
+  
+  /**
+   * @param args
+   * @throws IOException if some error occurs while crating output file
+   */
+  public static void main(String[] args) throws IOException
+  {
+    if (args.length < 2)
+    {
+      help("Wrong arguments (expected:2, given:"+args.length+")");
+      help();
+    }
+
+    final String jarFileName = args[0];
+    final String adlFileName = args[1];
+    if (jarFileName != null)
+    {
+      if (!jarFileName.endsWith(".jar"))
+      {
+        help("Input file is not a jar archive");
+      }
+      else if (!adlFileName.endsWith(".fractal"))
+      {
+        help("Output file is not a valid fractal file");
+      }
+    }
+
+    Fractalizer fractalizer = new Fractalizer();
+    String adlAsString = fractalizer.fractalize(jarFileName);
+    fractalizer.writeAdlOn(adlAsString, adlFileName);
+  }
+
+  private static void help(String message) {
+    System.out.println(message);
+    help();
+  }
+
+  /**
+   * Print usage and exit badly.
+   */
+  private static void help()
+  {
+    System.out.println("* Fractalizer - Help *");
+    System.out.println("Usage: \n java -jar fractalizer-${current.version} [inputFile.jar] [outputAdlFile.fractal]");
+    System.exit(1);
+  }
+
+}
---
+++ src/main/java/org/objectweb/fractal/fractalizer/JarClassLoader.java
@@ -0,0 +1,48 @@
+
+package org.objectweb.fractal.fractalizer;
+
+import java.util.Set;
+
+public class JarClassLoader extends MultiClassLoader
+{
+  private JarResources jarResources;
+
+  public JarClassLoader(String jarName)
+  {
+    // Create the JarResource and suck in the jar file.
+    jarResources = new JarResources(jarName);
+  }
+
+  protected byte[] loadClassBytes(String className)
+  {
+    // Support the MultiClassLoader's class name munging facility.
+    className = formatClassName(className);
+    // Attempt to get the class data from the JarResource.
+    return (jarResources.getResource(className));
+  }
+  
+  protected Set<String> classes() {
+    return this.jarResources.classes();
+  }
+  
+  public static void main(String[] args) throws Exception
+  {
+  if (args.length != 2)
+  {
+  System.err.println
+      ("Usage: java JarClassLoader " +
+       "<jar file name> <class name>");
+  System.exit (1);
+  }
+  /*
+   * Create the jar class loader and use the first argument
+   * passed in from the command line as the jar file to use.
+   */
+  JarClassLoader jarLoader = new JarClassLoader (args [0]);
+  /* Load the class from the jar file and resolve it. */
+  
+  
+  Class c = jarLoader.loadClass (args [1], true);
+  
+  }
+}
